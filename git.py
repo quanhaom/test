c7babc8c0ec4c6be6201ded12e7c1560410fd650
quanhaom <quan67100@gmail.com>
2018-12-09 17:56:07 +0700

    1

--- git.py
+++ git.py
@@ -1,9 +1,11 @@
 import os,random
+print(os.getcwd())  # In ra thư mục hiện tại
+
 for i in range (20):
     d = str(i) + 'days ago'
     rand = random.randrange(1,12)
     with open('test.txt','a') as file:
         file.write('iuhuyen'+'\n')
-    os.system('git add text.txt')
+    os.system('git add .')
     os.system('git commit --date=" 2018-' +str(rand) + '-'+d+'" -m 1')
     os.system('git push -u origin master -f')
