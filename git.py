b0a478a3087c3319821211e1331a6081bb2ba7b5
quanhaom <quan67100@gmail.com>
2024-12-14 17:54:37 +0700

    first

---
+++ git.py
@@ -0,0 +1,9 @@
+import os,random
+for i in range (20):
+    d = str(i) + 'days ago'
+    rand = random.randrange(1,12)
+    with open('test.txt','a') as file:
+        file.write('iuhuyen'+'\n')
+    os.system('git add text.txt')
+    os.system('git commit --date=" 2018-' +str(rand) + '-'+d+'" -m 1')
+    os.system('git push -u origin master -f')
