44e1e492b967b7e2d439221ce56fd053f3a96b38
quanhaom <quan67100@gmail.com>
2025-08-06 14:52:53 +0700

    1

--- git.py
+++ git.py
@@ -6,5 +6,5 @@ for i in range (1000):
     with open('test.txt','a') as file:
         file.write('iuhuyen'+'\n')
     os.system('git add .')
-    os.system('git commit --date=" 2018-' +str(rand) + '-'+d+'" -m 1')
+    os.system('git commit --date=" 2025-' +str(rand) + '-'+d+'" -m 1')
     os.system('git push -u origin master ')
